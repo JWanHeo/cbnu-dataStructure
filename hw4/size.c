4f6a9a5ee49b474a6a9ded242e0539aefa03d70a
jewanHeo <jewanheo@gmail.com>
2024-04-03 23:26:21 +0900

    struct.c 오류 코드 수정 및 학번 이름 출력 코드 추가

---
+++ hw4/size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+  int **x; // int형 2중 포인터 x 선언
+
+  printf("[----- 허제완 2020069031 -----]\n");
+
+  printf("sizeof(x) = %lu\n", sizeof(x)); // x의 크기 출력
+  printf("sizeof(*x) = %lu\n", sizeof(*x)); // x가 가리키는 값의 크기 출력
+  printf("sizeof(**x) = %lu\n", sizeof(**x)); // *x가 가리키는 값 출력
+
+}
