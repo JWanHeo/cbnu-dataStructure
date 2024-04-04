592f6d76e65c74d52b20a3dbf08eea584a8efd6d
jewanHeo <jewanheo@gmail.com>
2024-04-04 14:44:51 +0900

    size.c 주석 추가

--- hw4/size.c
+++ hw4/size.c
@@ -7,8 +7,8 @@ void main()
 
   printf("[----- 허제완 2020069031 -----]\n");
 
-  printf("sizeof(x) = %lu\n", sizeof(x)); // x의 크기 출력
-  printf("sizeof(*x) = %lu\n", sizeof(*x)); // x가 가리키는 값의 크기 출력
-  printf("sizeof(**x) = %lu\n", sizeof(**x)); // *x가 가리키는 값 출력
+  printf("sizeof(x) = %lu\n", sizeof(x)); // x의 크기(포인터의 크기) 출력
+  printf("sizeof(*x) = %lu\n", sizeof(*x)); // x가 가리키는 값의 크기(포인터의 크기) 출력
+  printf("sizeof(**x) = %lu\n", sizeof(**x)); // *x가 가리키는 값(int의 크기) 출력
 
 }
