5a402e37c155e1acf093374c11649d1581b69615
jewanHeo <jewanheo@gmail.com>
2024-04-04 14:45:49 +0900

    padding.c 주석 추가

--- hw4/padding.c
+++ hw4/padding.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-struct student {
-  char lastName[13]; /* 13 bytes */
-  int studentId; /* 4 bytes */
-  short grade; /* 2 bytes */
+struct student { // 구조체 student 정의
+  char lastName[13]; /* 13 bytes */ // 문자열 멤버변수 lastName
+  int studentId; /* 4 bytes */ // int형 멤버변수 studentId
+  short grade; /* 2 bytes */ // short형 멤버변수 grade
 };
 
 int main()
 {
-  struct student pst;
+  struct student pst; // student의 인스턴스 pst 선언
 
   printf("[----- 허제완 2020069031 -----]\n");
   
-  printf("size of student = %ld\n", sizeof(struct student));
-  printf("size of int = %ld\n", sizeof(int));
-  printf("size of short = %ld\n", sizeof(short));
+  printf("size of student = %ld\n", sizeof(struct student)); // student 구조체의 크기 출력
+  printf("size of int = %ld\n", sizeof(int)); // int의 크기 출력 
+  printf("size of short = %ld\n", sizeof(short)); // short의 크기 출력
   
   return 0;
 }
